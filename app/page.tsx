694c315637dfdbafd71aa447ac47efeba993b886
anonim-cmyk <ilhamsyahdan123@gmail.com>
2025-06-15 21:07:45 +0700

    Implemented Auth, Billing, Database setup, Create Companion Server Action

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import Navbar from "@/components/Navbar";
+import CompanionCards from "@/components/CompanionCards";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 const Page = () => {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCards
+          id="123"
+          name="Neura the Brainly Explorer"
+          topic="Neural Network of the brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCards
+          id="456"
+          name="Countsy the number wizard"
+          topic="Derivatives & integrals"
+          subject="Math"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCards
+          id="789"
+          name="Verba the builder"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
